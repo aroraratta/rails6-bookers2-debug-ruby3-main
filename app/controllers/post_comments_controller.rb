68ce159a8cc60d557f947672d480e47acfa3ae52
aroraratta <177751775+aroraratta@users.noreply.github.com>
2024-10-21 08:38:18 +0000

    [Error]undefined method book_book_comments_pathだがそんなパスの記述はしていない

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,16 +1,18 @@
 class PostCommentsController < ApplicationController
-  def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(post_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book.id)
+  
+  class PostCommentsController < ApplicationController
+    def create
+      book = Book.find(params[:book_id])
+      comment = current_user.book_comments.new(post_comment_params)
+      comment.book_id = book.id
+      comment.save
+      redirect_to book_path(book.id)
+    end
+  
+    private
+  
+    def post_comment_params
+      params.require(:book_comment).permit(:comment)
+    end
   end
-
-  private
-
-  def post_comment_params
-    params.require(:book_comment).permit(:comment)
-  end
-
 end
