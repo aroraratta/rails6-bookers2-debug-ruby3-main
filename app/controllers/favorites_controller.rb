fc4850596898986029b0643666df8808eea002dc
aroraratta <177751775+aroraratta@users.noreply.github.com>
2024-10-26 04:28:12 +0000

    [Add]favoritesコントローラーへjsレスポンスの記述

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,22 @@ class FavoritesController < ApplicationController
   
   def create
     book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
+    favorite = current_user.favorites.new
+    if favorite.save
+      respond_to do |format|
+        format.js
+      end
+    end
   end
   
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
-    favorite.destroy
+    if favorite.destroy
+      respond_to do |format|
+        format.js
+      end
+    end
   end
   
 end
