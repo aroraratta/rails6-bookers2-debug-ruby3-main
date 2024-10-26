81d15a332c984dfece9576b9ba6eac9f5ea5be64
aroraratta <177751775+aroraratta@users.noreply.github.com>
2024-10-26 05:10:03 +0000

    [Update]favorites_controller.rbのリダイレクト先削除

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,21 +3,12 @@ class FavoritesController < ApplicationController
   def create
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new
-    if favorite.save
-      respond_to do |format|
-        format.js
-      end
-    end
+    favorite.save
   end
   
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
-    if favorite.destroy
-      respond_to do |format|
-        format.js
-      end
-    end
+    favorite.destroy
   end
-  
 end
