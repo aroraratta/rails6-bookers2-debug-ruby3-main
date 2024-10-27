b3943f8efcb1876c9af7af6466751963dfcbfa44
aroraratta <177751775+aroraratta@users.noreply.github.com>
2024-10-27 05:50:44 +0000

    [Add]ユーザー詳細画面へDMリンク追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
         @currentUserEntry.each do |cu|
           @userEntry.each do |u|
             if cu.room_id == u.room_id then
-              @isEoom = true
+              @isRoom = true
               @roomId = cu.room_id
             end
           end
