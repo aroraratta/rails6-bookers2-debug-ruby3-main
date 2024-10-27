6ba0de249d475ca1a3b79841822586f2767c6e07
aroraratta <177751775+aroraratta@users.noreply.github.com>
2024-10-27 03:31:59 +0000

    [Add]コントローラーの追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,5 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
   
-  validates :message, presence: true, length: { macmum: 140 
+  validates :message, presence: true, length: { macmum: 140 }
 end
