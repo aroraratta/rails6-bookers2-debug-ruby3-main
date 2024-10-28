5af4b3916a11704c1a741817edf2d9845e816edf
aroraratta <177751775+aroraratta@users.noreply.github.com>
2024-10-28 02:47:44 +0000

    [Start]応用課題9a開始

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_26_071328) do
+ActiveRecord::Schema.define(version: 2024_10_28_024307) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -80,6 +80,13 @@ ActiveRecord::Schema.define(version: 2024_10_26_071328) do
     t.text "comment"
     t.integer "user_id"
     t.integer "book_id"
+  end
+
+  create_table "read_counts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "relationships", force: :cascade do |t|
