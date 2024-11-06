e823ae2c0bd38a00e95369c25099b460f6eaebac
EC2 Default User <ec2-user@ip-172-31-34-246.ap-northeast-1.compute.internal>
2024-11-06 08:28:52 +0000

    [Add]モデルへgusestメソッドの定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,4 +51,13 @@ class User < ApplicationRecord
     end
   end
 
+  GUEST_USER_EMAI = "guest@example.com"
+
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+
 end
