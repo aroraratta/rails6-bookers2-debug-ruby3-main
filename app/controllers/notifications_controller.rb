655b8410244e68ec2f4a0d9fd9d34f52be34fd5e
EC2 Default User <ec2-user@ip-172-31-34-246.ap-northeast-1.compute.internal>
2024-11-07 06:19:21 +0000

    [Add]ヘルパーの作成

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,14 @@
 class NotificationsController < ApplicationController
+
+  def update
+    notification = current_user.notifications.find(params[:id])
+    notification.update(read: true)
+    case notification.notifiable
+    when "Book"
+      redirect_to book_path(notification.notifiable)
+    else
+      redirect_to user_path(notification.notifiable.user)
+    end
+  end
+
 end
