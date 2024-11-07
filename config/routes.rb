752e717b3007ddfb641ec8b4c1d2bed4a1f8e7dc
EC2 Default User <ec2-user@ip-172-31-34-246.ap-northeast-1.compute.internal>
2024-11-07 06:22:09 +0000

    [Add]notification#updateのルート作成

--- config/routes.rb
+++ config/routes.rb
@@ -25,4 +25,6 @@ Rails.application.routes.draw do
   resources :rooms, only: [:create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :notification, only: [:update]
+
 end
