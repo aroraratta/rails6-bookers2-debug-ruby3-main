6592c8b05f58f3af7c859ce615ed65807d081755
EC2 Default User <ec2-user@ip-172-31-34-246.ap-northeast-1.compute.internal>
2024-11-07 04:12:15 +0000

    [Add]通知機能の実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,6 +8,12 @@ class Book < ApplicationRecord
   
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+  after_create do
+    user.followers.each do |follower|
+      notifications.create(user_id: follower.id)
+    end
+  end
   
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
