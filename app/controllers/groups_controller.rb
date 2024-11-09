7a5854001de63acb49463e26df09477eae3d159b
EC2 Default User <ec2-user@ip-172-31-34-246.ap-northeast-1.compute.internal>
2024-11-09 06:20:53 +0000

    [Add]group#new定義、ビューファイル作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,4 +5,25 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
 
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.owner_id = current_user.id
+    if @group.save
+      redirect_to groups_path
+    else
+      render 'new'
+    end
+  end
+
+  
+  private
+  
+  def group_params
+    params.require(:group).permit(:name, :introduction, :group_image)
+  end
+
 end
