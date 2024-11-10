8dd356d5679b956041cabdb88a2a1295bce06c73
EC2 Default User <ec2-user@ip-172-31-34-246.ap-northeast-1.compute.internal>
2024-11-10 05:12:49 +0000

    [Finish]応用課題9a終了

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,8 @@ Rails.application.routes.draw do
 
   resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
     resource :group_users, only: [:create, :destroy]
+    get "new_mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
   end
 
 end
